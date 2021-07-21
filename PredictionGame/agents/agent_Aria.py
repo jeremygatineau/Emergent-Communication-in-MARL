776210a344d4a2507dd85797ab1ceb841916bd5e
JohnSaliver <51422732+JohnSaliver@users.noreply.github.com>
2021-07-21 17:05:20 -0400

    eg

--- PredictionGame/agents/agent_Aria.py
+++ PredictionGame/agents/agent_Aria.py
@@ -7,11 +7,17 @@ class Aria(nn.Module):
     def __init__(self):
         super(Aria).__init__()
 
-        obs_Mod = None
-        action_Mod = None
-        msg_Enc = None
-        msg_Dec = None
-        rep_Mod = None
-
-    def forward(self, obs, msg):
-        return 
+        self.obs_Mod = None
+        self.action_Mod = None
+        self.msg_Enc = None
+        self.msg_Dec = None
+        self.rep_Mod = None
+        self.zObs = None
+    def embed_Obs(self, obs):
+        self.zObs = self.obs_Mod(obs) 
+    def forward(self, msg):
+        zMsg = self.msg_Enc(msg)
+        z = self.rep_Mod(torch.concat((zMsg, self.zObs)))
+        action = self.action_Mod(z)
+        message = self.msg_Dec(z)
+        return action, message
