fa8af5d995e82964c321e7aa11accc619e328f0d
JohnSaliver <51422732+JohnSaliver@users.noreply.github.com>
2021-07-21 16:04:53 -0400

    Create agent_Aria.py

---
+++ PredictionGame/agents/agent_Aria.py
@@ -0,0 +1,17 @@
+import torch
+import torchvision
+import torch.nn as nn
+import numpy as np
+
+class Aria(nn.Module):
+    def __init__(self):
+        super(Aria).__init__()
+
+        obs_Mod = None
+        action_Mod = None
+        msg_Enc = None
+        msg_Dec = None
+        rep_Mod = None
+
+    def forward(self, obs, msg):
+        return 
