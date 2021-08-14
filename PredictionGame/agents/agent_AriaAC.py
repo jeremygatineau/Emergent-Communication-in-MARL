d5c82bbb17519ad8ebec93cc9e3c55e6552a3dab
JohnSaliver <51422732+JohnSaliver@users.noreply.github.com>
2021-08-14 10:52:39 -0400

    you are the target of my policy *_* sdkjfng stfu

--- PredictionGame/agents/agent_AriaAC.py
+++ PredictionGame/agents/agent_AriaAC.py
@@ -5,9 +5,8 @@ import numpy as np
 from torch.distributions import Categorical
 import torch.nn.utils as utils
 from torch.autograd import Variable
-class AriaAC(nn.Module):
-    def __init__(self,opt_params, with_memory=False, aidi=None):
-        super(AriaAC, self).__init__()
+class AriaAC:
+    def __init__(self,opt_params, with_memory=True, aidi=None):
         self.aidi = aidi
         self.batch_size = opt_params["batch_size"]
         self.gamma = opt_params["gamma"]
@@ -16,25 +15,18 @@ class AriaAC(nn.Module):
         self.replay_size = opt_params["replay_size"]
         self.training_loops = opt_params["training_loops"]
         self.eps = np.finfo(np.float32).eps.item()
-        self.hiddenDim = 8
-        self.memory_size = 8
         self.with_memory = with_memory
-        self.obs_Mod = lin_Mod([2, self.hiddenDim//2])
-        self.msg_Enc = lin_Mod([4, self.hiddenDim//2, self.hiddenDim//2], sftmx = False)
-        self.rep_Mod = lin_Mod([self.hiddenDim, self.hiddenDim])
+        
+        self.modI = ariaModel(batch_size=self.batch_size, vocabulary_size=self.vocabulary_size, with_memory=self.with_memory).eval()
+        self.modT = ariaModel(batch_size=self.batch_size, vocabulary_size=self.vocabulary_size, with_memory=self.with_memory).train()
         if self.with_memory:
-            self.memory = nn.LSTMCell(self.hiddenDim, self.memory_size)
-            self.memories = [torch.zeros([1, 2*self.memory_size], dtype=torch.float32) for _ in range(self.batch_size+1)]
-            self.action_Mod = nn.Sequential(lin_Mod([self.memory_size, 1], sftmx = False), nn.Sigmoid())
-            self.msg_Dec = lin_Mod([self.memory_size, self.memory_size, self.vocabulary_size], sftmx=True)
-            self.value_head = lin_Mod([self.memory_size, self.memory_size, 1])
-        else : 
-            self.memory = None
-            self.memories = None
-            self.action_Mod = lin_Mod([self.hiddenDim, 1], sftmx = True)
-            self.value_head = lin_Mod([self.hiddenDim, 1])
+            self.memories_ongoing = torch.zeros([1, 2*self.memory_size], dtype=torch.float32)
+            self.memories_inital = torch.zeros([1, 2*self.memory_size], dtype=torch.float32)
+        else:
+            self.memories_ongoing = None
+            self.memories_inital = None
 
-        self.optimizer = torch.optim.Adam(self.parameters(), lr=opt_params["lr"])
+        self.optimizer = torch.optim.Adam(self.modT.parameters(), lr=opt_params["lr"])
 
         self.saved_act_Logp = []
         self.saved_values = []
@@ -44,23 +36,10 @@ class AriaAC(nn.Module):
         self.saved_rewards = []
         self.saved_obs = []
         self.saved_downlink_msgs = []
+        self.minibatch_counter = 0
         self.batch_counter = 0
         
-    def forward(self, obs, msg, memory):
-        o = self.obs_Mod(obs)
-        m = self.msg_Enc(msg)
-        z = self.rep_Mod(torch.cat([o, m], -1))
-        if self.with_memory:
-            hz, cz = self.memory(z, (memory[:, :self.memory_size], memory[:, self.memory_size:]))
-            
-            out_memory = torch.cat([hz, cz], dim=1)
-        else:
-            hz = z
-            out_memory = None
-        action = self.action_Mod(hz)
-        message = self.msg_Dec(hz)
-        value = self.value_head(hz)
-        return action, message, out_memory, value
+    
 
     def select_action(self, obs, msg):
 
@@ -68,26 +47,41 @@ class AriaAC(nn.Module):
         msg_t = torch.tensor([msg], dtype=torch.float32)
         
         if self.with_memory:
-            action, message, hid_state, value = self.forward(obs_t.float(), msg_t.float(), self.memories[self.batch_counter])
-            self.memories[self.batch_counter+1] = hid_state
+            action, message, hid_state, _ = self.modI.forward(obs_t.float(), msg_t.float(), self.memories_ongoing)
+            self.memories_ongoing = hid_state.detach()
         else:
-            action, message, _, value = self.forward(obs_t.float(), msg_t.float(), None)
+            action, message, _, _ = self.modI.forward(obs_t.float(), msg_t.float(), None)
 
+        a_distrib = Categorical(torch.cat([action, 1-action], -1))
+        m_distrib = Categorical(message)
+        a = torch.argmax(a_distrib.probs, dim=-1).int()
+        m = torch.argmax(m_distrib.probs, dim=-1).int()
+        return a, m
+    
+    def select_actionTraing(self, obs, msg, last_state):
+    
+        obs_t = torch.tensor([obs], dtype=torch.float32)
+        msg_t = torch.tensor([msg], dtype=torch.float32)
+        
+        if self.with_memory:
+            action, message, hid_state, value = self.modT.forward(obs_t.float(), msg_t.float(), last_state)
+        else:
+            action, message, _, value = self.modT.forward(obs_t.float(), msg_t.float(), None)
+        
         a_distrib = Categorical(torch.cat([action, 1-action], -1))
         m_distrib = Categorical(message)
         a = a_distrib.sample()
         m = m_distrib.sample()
         a_entropy = a_distrib.entropy() 
         m_entropy = m_distrib.entropy()
-        self.pushBufferSelect(a_distrib.log_prob(a), m_distrib.log_prob(m), value, a_entropy + m_entropy)
-        return a, m
+        return a_distrib.log_prob(a), m_distrib.log_prob(m), value, hid_state, a_entropy + m_entropy
 
 
     def train_on_batch(self, state, reward):
         #self.popBuffer()
         self.pushBufferEpisode(state[0], state[1], reward)
-        self.batch_counter += 1
-        if self.batch_counter >= self.batch_size:
+        self.minibatch_counter += 1
+        if self.minibatch_counter >= self.batch_size:
             
             self.optimizer.zero_grad()
             returns = self.getReturns(normalize=True)
@@ -95,23 +89,29 @@ class AriaAC(nn.Module):
             rewards = torch.tensor(self.saved_rewards)
             policy_loss = 0
             value_loss = 0
+            entropy_loss = 0
+            last_state = self.memories_inital.detach()
+
             for i in range(self.batch_size-1):
-                advantage = rewards[i]+self.saved_values[i+1].item()-self.saved_values[i].item()
-                policy_loss += -(self.saved_act_Logp[i] + self.saved_msg_Logp[i])*advantage.detach()        
+                a_lp, m_lp, val, hid_state, entropy = self.select_actionTraing(self.saved_obs[i], self.saved_downlink_msgs[i], last_state)
+                last_state = hid_state
+                
+                advantage = returns[i]-val.item()
+                policy_loss += -(a_lp + m_lp)*advantage.detach()        
                 value_loss += advantage.pow(2)
-            """for a_logp, m_logp, ret, val, entro in zip(self.saved_act_Logp, self.saved_msg_Logp, returns, self.saved_values, self.saved_entropies):
-                advantage = ret - val.item()
-                policy_loss += -(a_logp + m_logp)*advantage.detach()        
-                value_loss += advantage.pow(2)"""
+                entropy_loss -= self.epsilon*entropy
+
             policy_loss /= self.batch_size
             value_loss /= self.batch_size
-            entropy_loss =  -self.epsilon*torch.cat(self.saved_entropies).sum()
             loss = policy_loss+ value_loss + entropy_loss
             loss.backward(retain_graph=True)
             self.optimizer.step()
             mean_policy = torch.cat(self.saved_act_Logp, 0).exp().mean(dim=0)
             rewards = np.copy(self.saved_rewards)
             self.reset_Buffer()
+            self.batch_counter+=1
+            if self.batch_counter%100==0:
+                self.modI.load_state_dict(self.modI.state_dict())
             return np.round([policy_loss.item(), value_loss.item(), entropy_loss.item()], 2), rewards, mean_policy
         return None, None, None
 
@@ -179,22 +179,39 @@ class lin_Mod(nn.Module):
         for m in self.mod:
             x_ = m(x_)
         return x_
-class ariaActor(nn.Module):
-    def __init__(self, hidden_dim=10):
-        super(ariaActor, self).__init__()
+class ariaModel(nn.Module):
+    def __init__(self, batch_size, vocabulary_size, hidden_dim=8, memory_size=8, with_memory=True):
+        super(ariaModel, self).__init__()
         self.hiddenDim = hidden_dim
-        self.obs_Mod = lin_Mod([2+self.hiddenDim, 5])
-        self.action_Mod = lin_Mod([self.hiddenDim, 2], sftmx = True)
-        self.msg_Enc = lin_Mod([4, 5], sftmx = False)
-        self.msg_Dec = lin_Mod([self.hiddenDim, 4], sftmx=True)
+        self.memory_size = memory_size
+        self.with_memory = with_memory
+        self.batch_size = batch_size
+        self.vocabulary_size = vocabulary_size
+        self.obs_Mod = lin_Mod([2, self.hiddenDim//2])
+        self.msg_Enc = lin_Mod([4, self.hiddenDim//2, self.hiddenDim//2], sftmx = False)
         self.rep_Mod = lin_Mod([self.hiddenDim, self.hiddenDim])
+        if self.with_memory:
+            self.memory = nn.LSTMCell(self.hiddenDim, self.memory_size)
+            self.action_Mod = nn.Sequential(lin_Mod([self.memory_size, 1], sftmx = False), nn.Sigmoid())
+            self.msg_Dec = lin_Mod([self.memory_size, self.memory_size, self.vocabulary_size], sftmx=True)
+            self.value_head = lin_Mod([self.memory_size, self.memory_size, 1])
+        else : 
+            self.memory = None
+            self.action_Mod = lin_Mod([self.hiddenDim, 1], sftmx = True)
+            self.value_head = lin_Mod([self.hiddenDim, 1])
        
-    def forward(self, obs, msg, last_state):
-        inO = torch.cat([obs, last_state[self.batch_counter]], -1)
-        o = self.obs_Mod(inO)
+    def forward(self, obs, msg, memory):
+        o = self.obs_Mod(obs)
         m = self.msg_Enc(msg)
-        new_state = self.rep_Mod(torch.cat([o, m], -1))
-        action = self.action_Mod(new_state)
-        message = self.msg_Dec(new_state)
-
-        return action, message, new_state
+        z = self.rep_Mod(torch.cat([o, m], -1))
+        if self.with_memory:
+            hz, cz = self.memory(z, (memory[:, :self.memory_size], memory[:, self.memory_size:]))
+            
+            out_memory = torch.cat([hz, cz], dim=1)
+        else:
+            hz = z
+            out_memory = None
+        action = self.action_Mod(hz)
+        message = self.msg_Dec(hz)
+        value = self.value_head(hz)
+        return action, message, out_memory, value
