60f5630e959570037215939e8267da9c0be838b1
JohnSaliver <51422732+JohnSaliver@users.noreply.github.com>
2021-08-23 14:25:15 -0400

    Update agent_AriaAC.py

--- PredictionGame/agents/agent_AriaAC.py
+++ PredictionGame/agents/agent_AriaAC.py
@@ -134,7 +134,7 @@ class AriaAC:
                     Fi_m = nn.functional.gumbel_softmax(message.log(), tau=1)[0]
                    
                     rho_m = torch.clamp(Fi_m[m]/Bi_m[m], min=0, max=5).detach()
-                    advantage = rewards[i]+val_-val
+                    advantage = rewards[i]+self.gamma*val_-val
                     policy_loss += -(Fi_a[a].log()+Fi_m[m].log())*advantage.detach() # GBS 
                     #policy_loss += -(Fi_a[a].log()*rho_a.detach()+Fi_m[m].log()*rho_m.detach())*advantage.detach()  # GSB prioritized sampling   
                     #policy_loss += -(a_lp+m_lp)*advantage.detach()  # straight AC
