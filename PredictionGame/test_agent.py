795a9545eb8ed88e1dfe7f61f3b33da76012ee67
JohnSaliver <51422732+JohnSaliver@users.noreply.github.com>
2021-08-25 21:05:30 -0400

    Update test_agent.py

--- PredictionGame/test_agent.py
+++ PredictionGame/test_agent.py
@@ -17,14 +17,14 @@ matplotlib.use('Agg')
 
 
 epochs = int(5e3)
-opt_params = {"lr":0.01, "batch_size":20, \
+opt_params = {"lr":0.001, "batch_size":20, \
               "gamma":0.9, "vocab_size":4, \
               "memory_size":10, "hidden_size": 10, \
               "eps":0.01, "cross_reward_coef":0}
 run = wandb.init(config=opt_params, project='EC-MARL TOY PB', entity='jjer125')
 
 agent0 = AriaAC(opt_params=opt_params, with_memory=True, aidi=0)
-agent1 = AriaAC(opt_params=opt_params, with_memory=True, aidi=1)
+agent1 = AriaAC(opt_params=opt_params, with_memory=True, aidi=1)    
 np.random.seed(1)
 field = OneDfield(speed=1)
 Task = ToyTask(field=field, observationMappingFct=lambda x: (x>0.5).astype(int), comChannel=TwoWayComChannel(), cross_r_coef=opt_params["cross_reward_coef"])
