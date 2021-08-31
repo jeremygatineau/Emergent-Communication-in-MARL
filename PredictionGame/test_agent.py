d2d01d98c2d97ebf103e6c664e04d6f01d14e536
JohnSaliver <51422732+JohnSaliver@users.noreply.github.com>
2021-08-30 23:15:30 -0400

    Update test_agent.py

--- PredictionGame/test_agent.py
+++ PredictionGame/test_agent.py
@@ -17,10 +17,10 @@ matplotlib.use('Agg')
 
 
 epochs = int(5e4)
-opt_params = {"lr":0.001, "batch_size":128, \
-              "gamma":0.9, "vocab_size":4, \
+opt_params = {"lr":0.0007, "batch_size":64, \
+              "gamma":0.9, "vocab_size":16, \
               "memory_size":20, "hidden_size": 20, \
-              "eps":0.01, "cross_reward_coef":1}
+              "eps":0.01, "cross_reward_coef":0.3}
 run = wandb.init(config=opt_params, project='EC-MARL TOY PB', entity='jjer125')
 
 agent0 = AriaAC(opt_params=opt_params, with_memory=True, aidi=0)
